54d71e94b353d2a2e85a6a83770b05feedfe3e9a
inghamn <inghamn@bloomington.in.gov>
2017-01-12 09:47:23 -0500

    Hello World

---
+++ src/Controller/DirectoryController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright 2017 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/agpl.txt GNU/AGPL, see LICENSE.txt
+ */
+namespace Drupal\directory\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class DirectoryController extends ControllerBase
+{
+    public function staff()
+    {
+        return [
+            '#type' => 'markup',
+            '#markup' => "<h2>Staff</h2>";
+        ];
+    }
+}
